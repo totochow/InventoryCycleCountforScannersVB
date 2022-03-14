c717525b2d9a8f22a70bbce0061bd27ce84eb04f
Tony Chow <totochow@hotmail.com>
2022-03-14 11:29:52 -0700

    Revert "Removed the styles"
    This reverts commit d88245d6c91475eb2b6558dc11d0d674195a6ff2.

--- Reopen_MO/App_Start/BundleConfig.vb
+++ Reopen_MO/App_Start/BundleConfig.vb
@@ -19,7 +19,7 @@ Public Module BundleConfig
                   "~/Scripts/bootstrap.js"))
 
         bundles.Add(New StyleBundle("~/Content/css").Include(
-                  "~/Content/bootstrap.css",
+                  "~/Content/bootstrap-Cerulean.css",
                   "~/Content/site.css"))
     End Sub
 End Module
